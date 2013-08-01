697abf5ede7ad671d064bd17a5f284ea66478f4f
Tom Powell <zifnab@zifnab06.net>
2013-08-01 00:48:28 -0600

    Finalized, works with 1.6.2

--- src/net/zifnab06/AdminHunt/AdminHunt.java
+++ src/net/zifnab06/AdminHunt/AdminHunt.java
@@ -10,24 +10,27 @@ import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.EventPriority;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.Projectile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.ChatColor;
 
+import com.sk89q.worldguard.protection.events.DisallowedPVPEvent;
 
-public class AdminHunt extends JavaPlugin {
+public class AdminHunt extends JavaPlugin implements Listener{
 	private List<String> enabledPlayers;
 
 	@Override
 	public void onDisable(){
 		getConfig().set("playerList", enabledPlayers);
 		saveConfig();
+		getLogger().info("AdminHunt has stopped :(");
 	}
 	@Override
 	public void onEnable(){
+		this.getServer().getPluginManager().registerEvents(this, this);
 		//Create new config file - only used for persistance between sessions
 		File config_file = new File(getDataFolder(), "config.yml");
 		if (!config_file.exists()) {
@@ -37,19 +40,31 @@ public class AdminHunt extends JavaPlugin {
 		}
 		//restore players from last session
 		enabledPlayers = getConfig().getStringList("playerList");
+		getLogger().info("AdminHunt has been started successfully.");
+
 	}
 	public boolean onCommand(CommandSender sender, Command command, String name, String[] args) {
-		if (command.getName().equalsIgnoreCase("pvp-toggle") && sender instanceof Player) {
+		if (command.getName().equalsIgnoreCase("adminhunt") && sender instanceof Player) {
 			if (args.length == 0) return false;
-			if(enabledPlayers.size() > 0)
-			sender.sendMessage(enabledPlayers.get(0));
 			if (sender.hasPermission("adminhunt.toggle")){
-				Player player = (Player) sender;
 				if (!enabledPlayers.contains(args[0])){
+					getServer().broadcastMessage(ChatColor.GREEN + "[AdminHunt] An admin hunt as begun! Find (and kill) " + args[0] + " as quickly as you can!" + ChatColor.RESET);
 					enabledPlayers.add(args[0]);
 				} else {
+					getServer().broadcastMessage(ChatColor.GREEN + "[AdminHunt] The admin hunt as ended. " + args[0] + " has been found." + ChatColor.RESET);
 					enabledPlayers.remove(args[0]);
 				}
+				return true;
+			}
+			return false;
+		}
+		if (command.getName().equalsIgnoreCase("adminhunt-list") && sender instanceof Player) {
+			if(sender.hasPermission("adminhunt.toggle.list")){
+				String playerList = "Enabled Players:";
+				for (String player : enabledPlayers){
+					playerList = playerList + " " + player;
+				}
+				sender.sendMessage(playerList);
 				return true;
 			}
 			return false;
@@ -57,43 +72,12 @@ public class AdminHunt extends JavaPlugin {
 		return false;
 
 	}
-	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = false)
-	private void onEntityDamageByProjectile(EntityDamageByEntityEvent event) {
-		Entity defender = event.getEntity();
-		Entity attacker = ((Projectile) event.getDamager()).getShooter();
-		if (defender instanceof Player && attacker instanceof Player){
-			Player player = (Player) defender;
-			String defenderName = player.getDisplayName();
-			if ( enabledPlayers.contains(defenderName)){
-				event.setCancelled(false);
-			}
+	@EventHandler(priority = EventPriority.LOWEST)
+	private void onPVPDamage(DisallowedPVPEvent event){
+		Player defender = event.getDefender();
+		Player attacker = event.getAttacker();
+		if (enabledPlayers.contains(defender.getName()) || enabledPlayers.contains(attacker.getName())) { //cancel event
+			event.setCancelled(true);
 		}
 	}
-	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = false)
-	private void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
-		if (event.getDamager() instanceof Projectile) {
-			onEntityDamageByProjectile(event);
-			return;
-		}
-		Entity defender = event.getEntity();
-		Entity attacker = event.getDamager();
-		if (defender instanceof Player && attacker instanceof Player){
-			Player player = (Player) defender;
-			String defenderName = player.getDisplayName();
-			if ( enabledPlayers.contains(defenderName)){
-				event.setCancelled(false);
-			}
-		}
-	}
-
 }
-
-
-
-
-
-
-
-
-
-
